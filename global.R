564e1859128147c25660873b68960bbc70dbe6c5
niklasvm <niklasvm@gmail.com>
2018-08-06 20:39:35 +0200

    clean up global.R file and reference credentials.R if available

--- global.R
+++ global.R
@@ -1,22 +1,27 @@
+# global.R is sourced when the application is lauched
 message('global.R sourced')
 
-#### -- Packrat Autoloader (version 0.4.9-3) -- ####
-source("packrat/init.R")
-#### -- End Packrat Autoloader -- ####
+# Application options ----
 
-# this file is sourced when the application is lauched
+# show application api inputs
+ask_api_credentials <- F
 
+# whether to load cached data (must have authenticated before)
+cache <- F
 
-# the following environment variables must be set. Uncomment the lines below and set explicitly:
-# TIP: to get the strava_app_url you can just click `Run App` in RStudio and it will be the URL in the address bar.
+# Load credentials if available ----
 
-
+# the following environment variables must be set in credentials.R
 # Sys.setenv(
 #   strava_app_name='xxxxx',
 #   strava_app_url = 'xxxxxx',
 #   strava_app_client_id  = 'xxxx',
 #   strava_app_secret = 'xxxx'
 # )
+
+try({
+  source('./credentials.R')
+})
 
 # dependencies
 library(shiny)
@@ -29,24 +34,6 @@ library(DT)
 library(leaflet)
 library(shinydashboard)
 library(shinythemes)
-
-# # validate environment variables are set
-# missing <- c('strava_app_url','strava_app_client_id','strava_app_secret') %>% 
-#   map_lgl(~ Sys.getenv(.x) == "") %>% 
-#   any
-# if (missing) stop('Please set all require environment variables')
-# 
-# # generate authentication link as set out at https://developers.strava.com/docs/authentication/
-# authorisation_url <-   glue('https://www.strava.com/oauth/authorize?client_id={Sys.getenv(\'strava_app_client_id\')}&response_type=code&redirect_uri={Sys.getenv(\'strava_app_url\')}&approval_prompt=auto&state=')
-  
-
-# Application options ----
-
-# show application api inputs
-ask_api_credentials <- T
-
-# whether to load cached data (must have authenticated before)
-cache <- F
 
 # file dependencies ----
 
